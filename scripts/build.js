3aafb3747d7428de60116c4e0c2a1a8935295042
Dmitry Marov <d.marov94@gmail.com>
2021-07-18 19:51:46 +0300

    scripts wip

--- scripts/build.js
+++ scripts/build.js
@@ -1,16 +1,35 @@
-const env = require('dotenv').config();
+const env = require('dotenv').config().parsed;
+const rimraf = require('rimraf');
+const path = require('path');
+const glob = require('glob');
 
-const cheatEngineBinary = env.CHAMD_CHEAT_ENGINE_BINARY_DIR;
-const driverName = env.CHAMD_DBK_DRIVER_NAME;
-const seed = env.CHAMD.seed;
-
+console.log(env);
 class Context {
 
-    static build() {
-        console.log(`Generating ${driverName} ...`);
+    // you probably don't need to change this
+    static buildMode = 'Release';
+    static buildDir = path.normalize(__dirname + '\\..\\build\\');
+    static cheatEngineBinary = env.CHAMD_CHEAT_ENGINE_BINARY_DIR;
+    static driverName = env.CHAMD_DBK_DRIVER_NAME;
+    static seed = env.CHAMD_SEED;
 
+    static async build() {
+        console.log(`Generating ${this.driverName} driver ...`);
+        await this.purgeBuildDir();
     }
 
+    static async purgeBuildDir() {
+
+        console.log(`Clearing ${this.buildDir}* ...`);
+
+        return new Promise((res, rej) => {
+            rimraf(`${this.buildDir}\\*`, () => {
+                res();
+            });
+        });
+
+
+    }
 }
 
-Context::build();
+Context.build();
