196ab7b089b56edc7fe83e06e2b61742de0142f2
Dmitry Marov <d.marov94@gmail.com>
2021-07-18 11:58:52 +0300

    removed unused files

--- src/sigcheck.h
+++
@@ -1,8 +0,0 @@
-#ifndef SIGCHECK_H
-#define SIGCHECK_H
-
-NTSTATUS SecurityCheck(void);
-NTSTATUS CheckSignatureOfFile(PUNICODE_STRING originalpath);
-NTSTATUS CheckSignature(PVOID buffer, DWORD buffersize, PVOID sig, DWORD sigsize);
-
-#endif
