87272fa0c8dbfc56d45a1f2c0e27ddb8bc1a7273
Dmitry Marov <d.marov94@gmail.com>
2021-07-18 20:40:46 +0300

    build script wip

--- scripts/build.js
+++ scripts/build.js
@@ -2,20 +2,31 @@ const env = require('dotenv').config().parsed;
 const rimraf = require('rimraf');
 const path = require('path');
 const glob = require('glob');
+const fs = require('fs');
+const handlebars = require('handlebars');
 
 console.log(env);
 class Context {
 
-    // you probably don't need to change this
+    // you probably don't need to change this settings
     static buildMode = 'Release';
     static buildDir = path.normalize(__dirname + '\\..\\build\\');
     static cheatEngineBinary = env.CHAMD_CHEAT_ENGINE_BINARY_DIR;
     static driverName = env.CHAMD_DBK_DRIVER_NAME;
     static seed = env.CHAMD_SEED;
 
+    static vcCommunityPath = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat';
+    static vcEnterprisePath = 'C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\\Auxiliary\\Build\\vcvarsall.bat';
+    static vcPath = null;
+
+    static cmakeTplPath = path.normalize(__dirname + '\\..\\templates\\CMakeLists.txt.tpl');
+    static srcDir = path.normalize(__dirname + '\\..\\src\\');
+    static cmakeConfigPath = this.srcDir + 'CMakeLists.txt';
+
     static async build() {
         console.log(`Generating ${this.driverName} driver ...`);
         await this.purgeBuildDir();
+        await this.generateInfFile();
     }
 
     static async purgeBuildDir() {
@@ -27,7 +38,31 @@ class Context {
                 res();
             });
         });
+    }
 
+    static async generateInfFile() {
+        const communityVs = fs.existsSync(this.vcvarsCommunity);
+        const enterpriseVs = fs.existsSync(this.vcvarsEnterprise);
+
+        if (!communityVs) {
+            this.vcPath = this.vcCommunityPath;
+        }
+
+        if (!enterpriseVs) {
+            this.vcPath = this.vcEnterprisePath;
+        }
+
+        if (this.vcPath === null) {
+            throw new Error('Visual studio not found');
+        }
+
+        const cmakeTemplateContent = fs.readFileSync(this.cmakeTplPath, 'utf-8');
+        const DRIVER_NAME = this.driverName;
+
+        const template = handlebars.compile(cmakeTemplateContent);
+        const res = template({ DRIVER_NAME })
+
+        fs.writeFileSync(this.cmakeConfigPath, res);
 
     }
 }
--- templates/CMakeLists.txt.tpl
+++ templates/CMakeLists.txt.tpl
@@ -1,5 +1,5 @@
 cmake_minimum_required(VERSION 3.18.0)
-project(chamd VERSION 1.0.0)
+project({{DRIVER_NAME}} VERSION 1.0.0)
 
 enable_language(C ASM_MASM)
 
@@ -9,7 +9,7 @@ string(REPLACE "/GR" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W1 /WX")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} /W1 /WX")
 
-wdk_add_driver(chamd KMDF 1.15
+wdk_add_driver({{DRIVER_NAME}} KMDF 1.15
     "${CMAKE_CURRENT_SOURCE_DIR}/DBKDrvr.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/DBKFunc.c"
     "${CMAKE_CURRENT_SOURCE_DIR}/debugger.c"
@@ -34,9 +34,9 @@ wdk_add_driver(chamd KMDF 1.15
     "${CMAKE_CURRENT_SOURCE_DIR}/amd64/vmxoffloada.asm"
 )
 
-target_include_directories(chamd PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}" PUBLIC "${CMAKE_CURRENT_BINARY_DIR}")
+target_include_directories({{DRIVER_NAME}} PUBLIC "${CMAKE_CURRENT_SOURCE_DIR}" PUBLIC "${CMAKE_CURRENT_BINARY_DIR}")
 set(EXECUTABLE_OUTPUT_PATH ${CMAKE_BINARY_DIR})
-set_target_propertieschamd(
+set_target_properties{{DRIVER_NAME}}(
     PROPERTIES
     RUNTIME_OUTPUT_DIRECTORY_RELEASE "${CMAKE_BINARY_DIR}"
     RUNTIME_OUTPUT_DIRECTORY_DEBUG "${CMAKE_BINARY_DIR}"
